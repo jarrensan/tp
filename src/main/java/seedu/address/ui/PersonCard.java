c0942d353b27d83b812b8a52e8391d1eb8377793
Jarren San <79039461+jarrensan@users.noreply.github.com>
2026-03-16 02:17:15 +0800

    Resolve merge conflicts

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -31,6 +31,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label name;
     @FXML
+    private Label age;
+    @FXML
     private Label id;
     @FXML
     private Label phone;
@@ -59,6 +61,7 @@ public class PersonCard extends UiPart<Region> {
         this.person = person;
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
+        age.setText(person.getAge().value);
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
